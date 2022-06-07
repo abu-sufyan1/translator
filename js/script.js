0411430820124c8a21b1e20fd3f25e33ffcb8b3d
Sufyan Shaikh <Sufyan.Shaikh@extentia.com>
2022-06-07 17:26:06 +0530

    backspace functionality added.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 var dropdownValues = [];
 var response = '';
 var cursorPosition = {};
-document.getElementById("langTextArea").addEventListener('keypress', (e) => {
 
-    
+// var mousePosition = {x:0, y:0};
+// document.addEventListener('mousemove', function(mouseMoveEvent){
+//   mousePosition.x = mouseMoveEvent.pageX;
+//   mousePosition.y = mouseMoveEvent.pageY;
+// }, false);
+
+document.getElementById("langTextArea").addEventListener('keydown', (e) => {
+
+
     if (e.key === " " ||
         e.code === "Space" ||
         e.keyCode === 32 ||
@@ -23,10 +30,10 @@ document.getElementById("langTextArea").addEventListener('keypress', (e) => {
                 data => data.json()).then(
                     data => {
                         console.log(data);
-                        console.log(data[1][0][1][1]);
+                        console.log(data[1][0][1][0]);
                         dropdownValues.push(data[1][0][1]);
 
-                        response = data[1][0][1][1] ? data[1][0][1][1] : data[1][0][1][0];
+                        response = data[1][0][1][0] ? data[1][0][1][0] : data[1][0][1][1];
                         if (translatedText !== "") {
                             translatedText = translatedText + response + " ";
                         }
@@ -39,12 +46,30 @@ document.getElementById("langTextArea").addEventListener('keypress', (e) => {
         }
     }
 
-    if(e.code === "Backspace") {
-        if(e.target.value ? e.target.value.length === 0 : 1) {
+    if (e.key === "Backspace" || e.keyCode === 8) {
+        if (e.target.value ? e.target.value.length === 0 : 1) {
             return;
         }
-        if(e.target.value.lastIndexOf(" ") === e.target.value.length - 1) {
+        if (e.target.value.lastIndexOf(" ") === e.target.value.length - 1) {
+            document.getElementById("dropdown").style.display = 'none';
             return;
+        }
+        else if (getSelectionText().length === e.target.value.length) {
+            // all text selected.
+
+            return;
+        }
+        else {
+            if (document.getElementById("dropdown").style.display === 'none') {
+                let value = document.getElementById('langTextArea').value;
+                value = value.substring(value.lastIndexOf(" ") + 1);
+                fillDropdown(value);
+                document.getElementById("dropdown").style.top = 160 + (cursorPosition.start/90) * 15 + 'px';
+                document.getElementById("dropdown").style.left = 150 + (cursorPosition.start * 10) + 'px';
+                document.getElementById("dropdown").style.display = 'block';
+                document.getElementById("dropdown").focus();
+                // document.getElementById("dropdown").click();
+            }
         }
     }
 })
@@ -62,12 +87,8 @@ document.getElementById("langTextArea").addEventListener('click', (e) => {
 });
 
 
-document.getElementById("dropdown").addEventListener("click", function() {
+document.getElementById("dropdown").addEventListener("change", function () {
     let translatedText = document.getElementById('langTextArea').value;
-    // translatedText = translatedText.substring(0, translatedText.lastIndexOf(response));
-    // response = document.getElementById("dropdown").value;
-    // translatedText = translatedText + document.getElementById("dropdown").value;
-    // document.getElementById('langTextArea').value = translatedText;
     let translatedTextBefore = translatedText.substring(0, cursorPosition.start);
     translatedTextBefore = translatedTextBefore.substring(0, translatedTextBefore.lastIndexOf(" "));
     let translatedTextAfter = translatedText.substring(cursorPosition.start);
@@ -76,13 +97,28 @@ document.getElementById("dropdown").addEventListener("click", function() {
     document.getElementById('langTextArea').value = translatedTextBefore + document.getElementById("dropdown").value + translatedTextAfter;
 });
 
+// document.getElementById("dropdown").addEventListener("onchange", function () {
+//     let translatedText = document.getElementById('langTextArea').value;
+//     let translatedTextBefore = translatedText.substring(0, cursorPosition.start);
+//     translatedTextBefore = translatedTextBefore.substring(0, translatedTextBefore.lastIndexOf(" "));
+//     let translatedTextAfter = translatedText.substring(cursorPosition.start);
+//     translatedTextAfter = translatedTextAfter.substring(translatedTextAfter.indexOf(" "));
 
-function fillDropdown (value) {
+//     document.getElementById('langTextArea').value = translatedTextBefore + document.getElementById("dropdown").value + translatedTextAfter;
+//     document.getElementById("dropdown").innerHTML = "";
+//     document.getElementById("dropdown").style.display = 'none';
+// });
+
+
+function fillDropdown(value) {
     let select = document.getElementById("dropdown");
     document.getElementById("dropdown").innerHTML = "";
     cursorPosition = getCursorPosition(document.getElementById("langTextArea"))
     let text = getTheWord(cursorPosition.start, value);
     let index = dropdownValues.findIndex(x => x.includes(text));
+    if(index === -1) {
+        index = dropdownValues.length -1;
+    }
     let dropdownValue = dropdownValues[index];
     for (let i = 0; i < dropdownValue.length; i++) {
         let option = document.createElement("option"),
@@ -111,14 +147,14 @@ function getCursorPosition(input) {
             var rng = input.createTextRange();
             rng.moveToBookmark(sel.getBookmark());
             for (var len = 0;
-                     rng.compareEndPoints("EndToStart", rng) > 0;
-                     rng.moveEnd("character", -1)) {
+                rng.compareEndPoints("EndToStart", rng) > 0;
+                rng.moveEnd("character", -1)) {
                 len++;
             }
             rng.setEndPoint("StartToStart", input.createTextRange());
             for (var pos = { start: 0, end: len };
-                     rng.compareEndPoints("EndToStart", rng) > 0;
-                     rng.moveEnd("character", -1)) {
+                rng.compareEndPoints("EndToStart", rng) > 0;
+                rng.moveEnd("character", -1)) {
                 pos.start++;
                 pos.end++;
             }
@@ -126,4 +162,14 @@ function getCursorPosition(input) {
         }
     }
     return -1;
+}
+
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
 }
